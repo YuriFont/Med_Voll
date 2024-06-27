638d8bce0f97bfc584ca4c0a92de4732ec69d83f
YuriFont <yurifontenelesilva180303@gmail.com>
2024-06-27 16:03:02 -0300

    Create POST in /consultas

--- api/src/main/java/med/voll/api/controller/ConsultaController.java
+++ api/src/main/java/med/voll/api/controller/ConsultaController.java
@@ -2,20 +2,22 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import med.voll.api.domain.medico.DadosCadastrarMedico;
-import med.voll.api.domain.medico.DadosDetalhamentoMedico;
-import med.voll.api.domain.medico.Medico;
-import med.voll.api.domain.medico.MedicoRepository;
-import med.voll.api.domain.usuario.DadosCadastrarUsuario;
-import med.voll.api.domain.usuario.Usuario;
-import med.voll.api.domain.usuario.UsuarioRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URI;
-
+@RestController
+@RequestMapping("/consultas")
 public class ConsultaController {
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados) {
+        System.out.println(dados);
+        return ResponseEntity.ok(new DadosAgendamentoConsulta(null, null, null, null));
+    }
+
 }
